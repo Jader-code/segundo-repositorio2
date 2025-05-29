a9a49140260d8a53635fdff642d9c10b993d6048
Jader-code <jader.ramos@escola.pr.gov.br>
2025-05-28 22:16:07 -0300

    Aula 28/05

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Oque é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'Oque é JavaScript?',
+    'JavaScript é uma linguagem de programação'
+)
